7ca25b2b3a383a2e3d4b3b55548c0b2d756b7eae
dansun2 <dansunmusik7@gmail.com>
2024-04-12 12:06:03 +0900

    240412

---
+++ 02_core/06_object-constructor/02_constructor-function/student.js
@@ -0,0 +1,13 @@
+class Student{
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+
+    getinfo(){
+        return `${this.name}의 나이는 ${this.age}입니다.`
+    }
+
+}
+
+export default Student;
